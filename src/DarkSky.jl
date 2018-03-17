959c11f155f27e834c1aaaf744549b8ccf513721
Ellis Valentiner <ellisvalentiner@gmail.com>
2018-03-17 12:22:41 -0400

    Add DarkSkyResponse type

--- src/DarkSky.jl
+++ src/DarkSky.jl
@@ -6,16 +6,31 @@ using HTTP
 using JSON
 using Base.Dates
 
-function _get_json(url::String)
+OptionalDict = Union{Dict, Void}
+
+struct DarkSkyResponse
+    latitude::Float64
+    longitude::Float64
+    timezone::String
+    currently::OptionalDict
+    minutely::OptionalDict
+    hourly::OptionalDict
+    daily::OptionalDict
+    alerts::OptionalDict
+    flags::OptionalDict
+end
+DarkSkyResponse(x::Dict) = DarkSkyResponse((get.(x, String.(fieldnames(DarkSkyResponse)), nothing))...)
+
+function _get_json(url::String, verbose::Bool)
     response = HTTP.get(url)
-    info(response)
+    verbose ? info(response) : nothing
     if response.status == 200
-        return JSON.Parser.parse(String(response.body))
+        return DarkSkyResponse(JSON.Parser.parse(String(response.body)))
     end
 end
 
-forecast(latitude::Float64, longitude::Float64) = _get_json("https://api.darksky.net/forecast/$(ENV["DARKSKY_API_KEY"])/$latitude,$longitude")
-forecast(latitude::Float64, longitude::Float64, time::DateTime) = _get_json("https://api.darksky.net/forecast/$(ENV["DARKSKY_API_KEY"])/$latitude,$longitude,$time")
+forecast(latitude::Float64, longitude::Float64; verbose::Bool=true) = _get_json("https://api.darksky.net/forecast/$(ENV["DARKSKY_API_KEY"])/$latitude,$longitude", verbse)
+forecast(latitude::Float64, longitude::Float64, time::DateTime; verbose::Bool=true) = _get_json("https://api.darksky.net/forecast/$(ENV["DARKSKY_API_KEY"])/$latitude,$longitude,$time", verbose)
 export forecast
 
 end # module
