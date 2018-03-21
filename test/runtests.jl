ee6006ff555c1796bf53c78ac680a020463900b9
Ellis Valentiner <ellisvalentiner@gmail.com>
2018-03-21 13:40:27 -0400

    America/New_York isn't America/New_York?

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,7 +9,7 @@ response = forecast(42.3601, -71.0589, DateTime(2018, 1, 1, 0, 0, 0));
 @test typeof(response) === DarkSky.DarkSkyResponse
 @test latitude(response) === 42.3601
 @test longitude(response) === -71.0589
-@test timezone(response) === "America/New_York"
+@test timezone(response) == "America/New_York"
 @test typeof(currently(response)) === Dict{String, Any}
 @test typeof(minutely(response)) === Dict{String, Any}
 @test typeof(daily(response)) === Dict{String, Any}
