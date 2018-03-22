6dcd64711ba5266ee2434f9d43e75b39c365f0e7
Ellis Valentiner <ellisvalentiner@gmail.com>
2018-03-22 14:45:52 -0400

    Add tests that got lost

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,6 +9,14 @@ if haskey(ENV, "DARKSKY_API_KEY")
     @test typeof(response) === DarkSky.DarkSkyResponse
     response = forecast(42.3601, -71.0589, DateTime(2018, 1, 1, 0, 0, 0));
     @test typeof(response) === DarkSky.DarkSkyResponse
+    @test latitude(response) === 42.3601
+    @test longitude(response) === -71.0589
+    @test timezone(response) == "America/New_York"
+    @test typeof(currently(response)) === Dict{String, Any}
+    @test typeof(minutely(response)) === Dict{String, Any}
+    @test typeof(daily(response)) === Dict{String, Any}
+    @test typeof(alerts(response)) === Array{Any, 1}
+    @test typeof(flags(response)) === Dict{String, Any}
     response = forecast(42.3601, -71.0589, DateTime(2018, 1, 1, 0, 0, 0), lang="es", units="si", exclude=["minutely"]);
     @test typeof(response) === DarkSky.DarkSkyResponse
 else
