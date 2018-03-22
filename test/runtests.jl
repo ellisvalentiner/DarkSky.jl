028791e3991ca9eaec31f642f29acfefccc9c59c
Ellis Valentiner <ellisvalentiner@gmail.com>
2018-03-22 16:31:36 -0400

    Fix tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,22 +3,19 @@ using Base.Test
 
 if haskey(ENV, "DARKSKY_API_KEY")
     info("Dark Sky API key was found in your system environment variables, running tests...")
-    response = forecast(42.3601, -71.0589);
-    @test typeof(response) === DarkSky.DarkSkyResponse
-    response = forecast(42.3601, -71.0589, exclude=["currently"], extend="hourly");
-    @test typeof(response) === DarkSky.DarkSkyResponse
+    @test isa(forecast(42.3601, -71.0589), DarkSky.DarkSkyResponse)
+    @test isa(forecast(42.3601, -71.0589, exclude=["currently"], extend="hourly"), DarkSky.DarkSkyResponse)
     response = forecast(42.3601, -71.0589, DateTime(2018, 1, 1, 0, 0, 0));
-    @test typeof(response) === DarkSky.DarkSkyResponse
+    @test isa(response, DarkSky.DarkSkyResponse)
     @test latitude(response) === 42.3601
     @test longitude(response) === -71.0589
     @test timezone(response) == "America/New_York"
-    @test typeof(currently(response)) === Dict{String, Any}
-    @test typeof(minutely(response)) === Dict{String, Any}
-    @test typeof(daily(response)) === Dict{String, Any}
-    @test typeof(alerts(response)) === Array{Any, 1}
-    @test typeof(flags(response)) === Dict{String, Any}
-    response = forecast(42.3601, -71.0589, DateTime(2018, 1, 1, 0, 0, 0), lang="es", units="si", exclude=["minutely"]);
-    @test typeof(response) === DarkSky.DarkSkyResponse
+    @test isa(currently(response), Dict{String, Any})
+    @test isa(minutely(response), Dict{String, Any})
+    @test isa(daily(response), Dict{String, Any})
+    @test isa(alerts(response), Array{Any, 1}) | isa(alerts(response), Void)
+    @test isa(flags(response), Dict{String, Any})
+    @test isa(forecast(42.3601, -71.0589, DateTime(2018, 1, 1, 0, 0, 0), lang="es", units="si", exclude=["minutely"]), DarkSky.DarkSkyResponse)
 else
     warn("Dark Sky API key was not found in your system environment variables, skipping tests...")
 end
