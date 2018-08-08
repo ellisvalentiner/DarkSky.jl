b4d9345a8faee334d4bf5e14d7e3f8a45d6149cc
Ellis Valentiner <ellisvalentiner@gmail.com>
2018-08-08 16:27:44 -0400

    Begin v0.2

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,9 @@
 using DarkSky
-using Base.Test
+using Test
+using Dates
 
 if haskey(ENV, "DARKSKY_API_KEY")
-    info("Dark Sky API key was found in your system environment variables, running tests...")
+    @info("Dark Sky API key was found in your system environment variables, running tests...")
     @test isa(forecast(42.3601, -71.0589), DarkSky.DarkSkyResponse)
     @test isa(forecast(42.3601, -71.0589, exclude=["currently"], extend="hourly"), DarkSky.DarkSkyResponse)
     response = forecast(42.3601, -71.0589, DateTime(2018, 3, 7, 0, 0, 0));
@@ -11,11 +12,11 @@ if haskey(ENV, "DARKSKY_API_KEY")
     @test longitude(response) === -71.0589
     @test timezone(response) == "America/New_York"
     @test isa(currently(response), Dict{String, Any})
-    @test isa(minutely(response), Void)
+    @test isa(minutely(response), Nothing)
     @test isa(daily(response), Dict{String, Any})
-    @test isa(alerts(response), Array{Any, 1}) | isa(alerts(response), Void)
+    @test isa(alerts(response), Array{Any, 1}) | isa(alerts(response), Nothing)
     @test isa(flags(response), Dict{String, Any})
-    @test isa(print(response), Void)
+    @test isa(print(response), Nothing)
     dict_response = forecast(42.3601, -71.0589, DateTime(2018, 3, 7, 0, 0, 0), out_type="");
     @test Dict(response) == dict_response
     @test Dict(response) == convert(Dict, response)
