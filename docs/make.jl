c146717fe10cf19a7616825e9042ed895b6ce7e0
Pascal Jufer <paescuj@users.noreply.github.com>
2018-10-31 16:22:39 +0100

    Update Documenter.jl to 0.20 (#17)
    LGTM. Thanks @paescuj !

--- docs/make.jl
+++ docs/make.jl
@@ -3,11 +3,11 @@ using DarkSky
 
 makedocs(
     modules = [DarkSky],
-    doctest = false
+    doctest = false,
+    format = :markdown
 )
 
 deploydocs(
     deps   = Deps.pip("pygments", "mkdocs", "mkdocs-material", "python-markdown-math"),
     repo   = "github.com/ellisvalentiner/DarkSky.jl.git",
-    julia  = "0.7"
 )
